aae08b659780929638ab96b7d0c5ebac01ea0968
Mehdi <mehdirefiyev177@gmail.com>
2023-04-28 19:53:56 +0400

    second

--- script/script.js
+++ script/script.js
@@ -10,6 +10,9 @@ window.onscroll = () => {                       // scroll edende navbar kicilsin
     }
 };
 
+header.onmouseover = () =>{ header.style.opacity = "1"; }
+header.onmouseleave = () =>{ header.style.opacity = "0.8"; }
+
 let upper = document.getElementById("upper").addEventListener('click' , upperfuc , true); // seyfenin ey yuxarina getsin deye
 function upperfuc(){window.scrollTo({ top: 0, behavior: 'smooth' });}
 
@@ -21,3 +24,10 @@ function feedbackfuc(){
     if(feedback_count % 2 != 0){feedback_card.style.display = "inline-block";}
     else{feedback_card.style.display = "none";}
 }
+
+let Submit_btn = document.getElementById("Submit_btn");
+let dropdown_list = document.getElementById("dropdown-menu");
+Submit_btn.addEventListener("mouseover" , submit_btn_over , true );
+function submit_btn_over(){dropdown_list.style.display = "inline-block";}
+Submit_btn.addEventListener("mouseleave", submit_btn_leave , true );
+function submit_btn_leave(){dropdown_list.style.display = "none";}
